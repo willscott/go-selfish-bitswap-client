b88bd735384111b0880d610de29e7d6c92e4f4d1
Will Scott <will.scott@protocol.ai>
2022-02-28 15:16:55 +0100

    get blocks. a minimal client

---
+++ cmd/getswap/main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/ipfs/go-cid"
+	"github.com/libp2p/go-libp2p"
+	"github.com/libp2p/go-libp2p-core/peer"
+	"github.com/multiformats/go-multiaddr"
+	"github.com/urfave/cli/v2"
+	bitswap "github.com/willscott/go-selfish-bitswap-client"
+)
+
+func main() {
+	app := &cli.App{
+		Name:  "getswap",
+		Usage: "Bitswap demonstration retrieval client",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:     "peer",
+				Aliases:  []string{"p"},
+				Required: true,
+			},
+		},
+		Action: Get,
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func Get(c *cli.Context) error {
+	// handle /parse cid
+	if c.Args().Len() == 0 {
+		return fmt.Errorf("no cid specified")
+	}
+	cidStr := c.Args().First()
+	cidParsed, err := cid.Parse(cidStr)
+	if err != nil {
+		return err
+	}
+
+	// handle / parse peer
+	mStr := c.String("peer")
+	ma, err := multiaddr.NewMultiaddr(mStr)
+	if err != nil {
+		return err
+	}
+	ai, err := peer.AddrInfoFromP2pAddr(ma)
+	if err != nil {
+		return err
+	}
+
+	// make host
+	host, err := libp2p.New()
+	if err != nil {
+		return err
+	}
+	host.Peerstore().AddAddr(ai.ID, ai.Addrs[0], time.Hour)
+
+	s := bitswap.New(host, ai.ID)
+	bytes, err := s.Get(cidParsed)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s", err)
+		os.Exit(1)
+	}
+	_, err = os.Stdout.Write(bytes)
+	return err
+}
---
+++ session.go
@@ -0,0 +1,285 @@
+package bitswap
+
+import (
+	"context"
+	"encoding/binary"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"sync"
+	"time"
+
+	"github.com/ipfs/go-cid"
+	"github.com/libp2p/go-libp2p-core/host"
+	"github.com/libp2p/go-libp2p-core/network"
+	"github.com/libp2p/go-libp2p-core/peer"
+	"github.com/libp2p/go-libp2p-core/protocol"
+	"github.com/multiformats/go-multihash"
+
+	bitswap_message_pb "github.com/willscott/go-selfish-bitswap-client/message"
+)
+
+// Session holds state for a related set of CID requests from a single remote peer
+type Session struct {
+	host.Host
+	peer     peer.ID
+	initated sync.Once
+
+	close   context.CancelFunc
+	conn    network.Stream
+	connErr error
+
+	// 1 message sent on the ready chan once the connection is established
+	ready chan struct{}
+
+	wants chan cid.Cid
+	lbuf  []byte
+
+	interestMtx sync.Mutex
+	interests   map[cid.Cid]func([]byte, error)
+}
+
+// New initiates a bitswap retrieval session
+func New(h host.Host, peer peer.ID) *Session {
+	return &Session{
+		Host:      h,
+		peer:      peer,
+		ready:     make(chan struct{}),
+		wants:     make(chan cid.Cid, 5),
+		lbuf:      make([]byte, binary.MaxVarintLen64),
+		interests: make(map[cid.Cid]func([]byte, error)),
+	}
+}
+
+var (
+	// ProtocolBitswapNoVers is a legacy bitswap protocol id
+	ProtocolBitswapNoVers protocol.ID = "/ipfs/bitswap"
+	// ProtocolBitswapOneZero is the prefix for the legacy bitswap protocol
+	ProtocolBitswapOneZero protocol.ID = "/ipfs/bitswap/1.0.0"
+	// ProtocolBitswapOneOne is the the prefix for version 1.1.0
+	ProtocolBitswapOneOne protocol.ID = "/ipfs/bitswap/1.1.0"
+	// ProtocolBitswap is the current version of the bitswap protocol: 1.2.0
+	ProtocolBitswap protocol.ID = "/ipfs/bitswap/1.2.0"
+)
+
+func (s *Session) connect() {
+	sessionCtx, cncl := context.WithCancel(context.Background())
+	s.close = cncl
+	go s.writeLoop(sessionCtx)
+
+	// todo: configuratble timeout
+	ctx, cncl := context.WithDeadline(context.Background(), time.Now().Add(10*time.Second))
+	defer cncl()
+	stream, err := s.Host.NewStream(ctx, s.peer, ProtocolBitswap, ProtocolBitswapOneZero, ProtocolBitswapOneOne, ProtocolBitswapNoVers)
+	s.connErr = err
+	s.conn = stream
+	if s.connErr != nil {
+		return
+	}
+	s.Host.SetStreamHandler(stream.Protocol(), s.onStream)
+
+	go s.readLoop(sessionCtx)
+	s.ready <- struct{}{}
+}
+
+func (s *Session) onStream(stream network.Stream) {
+	buf := make([]byte, 4*1024*1024)
+	for {
+		//s.conn.SetReadDeadline(time.Now().Add(100 * time.Millisecond))
+		n, err := stream.Read(buf)
+		if err != nil {
+			if os.IsTimeout(err) {
+				continue
+			}
+			if errors.As(err, &io.EOF) {
+				return
+			}
+			//otherwise assume real error / conn closed.
+			s.connErr = err
+			s.Close()
+			return
+		}
+		if n > 0 {
+			s.handle(buf[:n])
+		}
+	}
+}
+
+// readLoop is the event loop handling inbound messages associated with this session.
+func (s *Session) readLoop(ctx context.Context) {
+	var buf []byte
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		default:
+		}
+
+		buf = make([]byte, 4*1024*1024)
+		//s.conn.SetReadDeadline(time.Now().Add(100 * time.Millisecond))
+		n, err := s.conn.Read(buf)
+		if err != nil {
+			if os.IsTimeout(err) {
+				continue
+			}
+			//otherwise assume real error / conn closed.
+			s.connErr = err
+			s.Close()
+			return
+		}
+		if n > 0 {
+			s.handle(buf[:n])
+		}
+	}
+}
+
+// writeLoop is the event loop handling outbound messages
+func (s *Session) writeLoop(ctx context.Context) {
+	cids := make([]cid.Cid, 0)
+	timeout := time.NewTicker(time.Millisecond * 50)
+	ready := false
+	defer close(s.ready)
+	defer timeout.Stop()
+	for {
+		select {
+		case c := <-s.wants:
+			cids = append(cids, c)
+		case <-s.ready:
+			ready = true
+			if len(cids) > 0 {
+				s.send(cids)
+				cids = make([]cid.Cid, 0)
+			}
+		case <-timeout.C:
+			if !ready {
+				continue
+			}
+			if len(cids) > 0 {
+				s.send(cids)
+				cids = make([]cid.Cid, 0)
+			}
+		case <-ctx.Done():
+			return
+		}
+	}
+}
+
+func (s *Session) send(cids []cid.Cid) error {
+	m := bitswap_message_pb.Message{}
+	m.Wantlist = bitswap_message_pb.Message_Wantlist{}
+	for _, c := range cids {
+		bc := bitswap_message_pb.Cid{Cid: c}
+		m.Wantlist.Entries = append(m.Wantlist.Entries, bitswap_message_pb.Message_Wantlist_Entry{Block: bc})
+	}
+
+	bytes, err := m.Marshal()
+	if err != nil {
+		return err
+	}
+
+	ln := binary.PutUvarint(s.lbuf, uint64(len(bytes)))
+	if _, err := s.conn.Write(s.lbuf[0:ln]); err != nil {
+		return err
+	}
+	if _, err := s.conn.Write(bytes); err != nil {
+		return err
+	}
+	return nil
+}
+
+// Handle an inbound message.
+func (s *Session) handle(buf []byte) {
+	ml, n := binary.Uvarint(buf)
+	if len(buf) < n+int(ml) {
+		fmt.Printf("partial read..\n")
+		return
+	}
+	m := bitswap_message_pb.Message{}
+	if err := m.Unmarshal(buf[n:]); err != nil {
+		//todo: log
+	}
+	// bitswap 1.1
+	for _, bp := range m.Payload {
+		prefix, err := cid.PrefixFromBytes(bp.Prefix)
+		if err != nil {
+			// todo: log
+			continue
+		}
+		c, err := prefix.Sum(bp.GetData())
+		if err != nil {
+			// todo: log
+			continue
+		}
+		s.resolve(c, bp.GetData(), nil)
+	}
+	// bitswap 1.0
+	for _, b := range m.Blocks {
+		// CIDv0, sha256, protobuf only
+		mh, err := multihash.Sum(b, multihash.SHA2_256, -1)
+		if err != nil {
+			// todo: log
+			continue
+		}
+		c := cid.NewCidV0(mh)
+		s.resolve(c, b, nil)
+	}
+}
+
+// Close stops the session.
+func (s *Session) Close() error {
+	if s.close != nil {
+		s.close()
+	}
+	if s.connErr != nil {
+		s.interestMtx.Lock()
+		defer s.interestMtx.Unlock()
+		for _, i := range s.interests {
+			i(nil, s.connErr)
+		}
+	}
+	return nil
+}
+
+func (s *Session) on(c cid.Cid, cb func([]byte, error)) {
+	s.interestMtx.Lock()
+	defer s.interestMtx.Unlock()
+	// todo: support multiple
+	s.interests[c] = cb
+}
+
+func (s *Session) resolve(c cid.Cid, data []byte, err error) {
+	s.interestMtx.Lock()
+	defer s.interestMtx.Unlock()
+
+	cb, ok := s.interests[c]
+	if ok {
+		delete(s.interests, c)
+		cb(data, err)
+	}
+}
+
+// Get a specific block of data in this session.
+func (s *Session) Get(c cid.Cid) ([]byte, error) {
+	// confirm connected.
+	s.initated.Do(s.connect)
+	if s.connErr != nil {
+		return nil, s.connErr
+	}
+	s.wants <- c
+
+	// wait for want to be handled.
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	var data []byte
+	var err error
+
+	s.on(c, func(rb []byte, re error) {
+		data = rb
+		err = re
+		wg.Done()
+	})
+	wg.Wait()
+
+	return data, err
+}
